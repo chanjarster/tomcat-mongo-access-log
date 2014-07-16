3b19b9416e7f6d8e4b8532855cc4acd176a9983c
Daniel Qian <chanjarster@gmail.com>
2014-07-16 20:10:31 +0800

    remove nodejs part

--- nodejs/bower_components/smooth-scroll/test/spec/spec-smoothScroll.js
+++
@@ -1,9 +0,0 @@
-describe('Smooth Scroll', function () {
-
-  describe('init', function () {
-    it('should include the smoothScroll module', function () {
-      expect(!!smoothScroll).toBe(true);
-    });
-  });
-
-});
